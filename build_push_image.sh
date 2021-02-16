975999543151575bb20d4ea7dcccc3ff3e4b14ff
deepak-muley <deepak.muley@gmail.com>
2021-02-16 11:12:43 -0800

    added push image code

---
+++ build_push_image.sh
@@ -0,0 +1,2 @@
+docker build -t deepakmuley/reactjs-video-player-webapp:latest .
+docker push deepakmuley/reactjs-video-player-webapp:latest
--- build_run_container.sh
+++ build_run_container.sh
@@ -1,2 +1,2 @@
-docker build -t video-player-app .
-docker run -it -p 3000:3000 video-player-app
+docker build -t deepakmuley/reactjs-video-player-webapp:latest .
+docker run -it -p 3000:3000 deepakmuley/reactjs-video-player-webapp:latest
